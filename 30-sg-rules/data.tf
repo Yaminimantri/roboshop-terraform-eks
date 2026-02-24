d333b217bc9dff3c8ef1369288b02a3d8a03e4f9
yamini <yamini.com>
2026-02-24 16:17:07 +0530

    jenkins

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -50,13 +50,13 @@ data "aws_ssm_parameter" "mysql_sg_id" {
 # }
 
 data "aws_ssm_parameter" "open_vpn_sg_id" {
-  name = "/${var.project_name}/${var.environment}/open_vpn_sg_id"
+  name = "/${var.project_name}/${var.environment}/open_vpn/sg_id"
 }
 
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
-  name = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
+  name = "/${var.project_name}/${var.environment}/eks_control_plane/sg_id"
 }
 
 data "aws_ssm_parameter" "eks_node_sg_id" {
-  name = "/${var.project_name}/${var.environment}/eks_node_sg_id"
+  name = "/${var.project_name}/${var.environment}/eks_node/sg_id"
 }
